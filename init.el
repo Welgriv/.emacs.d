d708be472bf9efdc4b29206b0e523506a0e6babc
Vincent Morice <vincent.morice@st.com>
2019-08-28 13:58:57 +0200

    enlève le une seule fenêtre pour latex

--- init.el
+++ init.el
@@ -84,8 +84,6 @@
 (split-window-right)
 
 ;; ---- LATEX
-;; enlève le full screen et garde un seul buffer ouvert.
-(add-hook 'LaTeX-mode-hook 'delete-other-windows)
 ;; mode mathe en mode lateX
 (add-hook 'LaTeX-mode-hook 'LaTeX-math-mode)
 ;; active flyspell
